b2e0838da41bb2243f93af8369f574722b957534
Benay Tomas <115767219+benaytms@users.noreply.github.com>
2026-03-08 02:44:35 +0000

    Implement rate limiting for image and word endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,28 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from backend.models import ImageResponse, WordResponse
 import backend.database as db
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
+# Slowapi added to avoid extreme rates to FastAPI
+
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI(title="APOD & Word of the Day API")
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 @app.get("/image/today", response_model=ImageResponse)
-def get_today_image():
+@limiter.limit("30/minute")
+def get_today_image(request: Request):
     image = db.get_today_image()
     if image is None:
         raise HTTPException(status_code=404, detail="No image for today yet")
     return image
 
 @app.get("/word/today", response_model=WordResponse)
-def get_today_word():
+@limiter.limit("30/minute")
+def get_today_word(request: Request):
     word = db.get_today_word()
     if word is None:
         raise HTTPException(status_code=404, detail="No word for today yet")
