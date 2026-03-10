c4454004890be40b391f972177233cf7ff474ff3
Benay Tomas <115767219+benaytms@users.noreply.github.com>
2026-03-10 18:23:17 +0000

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ NASA_API = str(os.getenv('NASA_API'))
 DATABASE_URL = str(os.getenv('DATABASE_URL'))
 DISCORD_WEBHOOK = str(os.getenv('DISCORD_WEBHOOK'))
 MERRIAM_KEY = str(os.getenv('MERRIAM_KEY'))
-APOD_URL = str(os.getenv('APOD_URL')) + NASA_API
+APOD_URL = "https://api.nasa.gov/planetary/apod?api_key=" + NASA_API
 
 def get_today()->str:
     return datetime.now(ZoneInfo("America/Sao_Paulo")).date().isoformat()
