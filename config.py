cbb49662d94d1ec94409a10c451e4ffd51be455d
Benay Tomas <115767219+benaytms@users.noreply.github.com>
2026-03-10 03:10:06 -0300

    Updated today to be a function

--- config.py
+++ config.py
@@ -5,8 +5,9 @@ import os
 
 load_dotenv()
 
-TODAY = datetime.now(ZoneInfo("America/Sao_Paulo")).date().isoformat()
 NASA_API = str(os.getenv('NASA_API'))
 DATABASE_URL = str(os.getenv('DATABASE_URL'))
 DISCORD_WEBHOOK = str(os.getenv('DISCORD_WEBHOOK'))
+def get_today() -> str:
+    return datetime.now(ZoneInfo("America/Sao_Paulo")).date().isoformat()
 
